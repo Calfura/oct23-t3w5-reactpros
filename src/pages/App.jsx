47cff83e0ded3c9a260bb364c3062583469204e6
Calfura <travis_wilson999@hotmail.com>
2024-05-02 19:57:35 +1000

    fix: validate on empty movies names

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,8 +1,12 @@
+import MovieList from '../components/MovieList';
 import '../stylesheets/App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Favourite Movies</h1>
+
+      <MovieList />
 
     </div>
   );
